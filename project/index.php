32ef75e7e45aa1c8942585e02ca32ae14a9c3065
Abdullah Öğük <aoguk1102@gmail.com>
2015-07-20 16:37:44 +0300

    1st

---
+++ project/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Soci al-Networking Site</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+</head>
+<body>
+<div class="container">
+	<div id="head_wrap">
+		<div id="header">
+			
+			<img src="images/logo.png" alt="" style="float:left"; padding: 5px;>
+			<form method="post" action="" id="form1">
+				<input type="text" name="email" placeholder = "Email"/>
+				<nput type = "password" name = "pass" placeholder = "*******"/>
+				<button name="login">Login</button>
+		</div>
+	</div>
+
+</div>
+
+
+
+</body>
+</html>
